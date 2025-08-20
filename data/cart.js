9aa2672f19093fc1eb3dff5da2f54aa407863c73
14th-August <cs.cfadams@gmail.com>
2025-08-19 18:10:54 -0700

    Current Lesson 14 Code

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,20 @@
-const cart = [];
+export const cart = [];
+
+export function addToCart(productId) {
+  let matchingItem;
+
+      cart.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+          matchingItem = cartItem; //how to figure out if item already in cart
+        }
+      });
+
+      if (matchingItem) {
+        matchingItem.quantity += 1;
+      } else {
+        cart.push({
+          productId: productId,
+          quantity: 1
+        });
+    }
+}
